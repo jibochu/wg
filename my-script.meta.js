8675647e79e393c17d3d4c4503c2f91129b48d87
jibochu <wangsicong360@gmail.com>
2026-08-07 20:56:20 +0800

    Update my-script.meta.js

--- my-script.meta.js
+++ my-script.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         WG
 // @namespace    http://tampermonkey.net/
-// @version      2.0.2
+// @version      2.0.3
 // @downloadURL  https://ghproxy.net/https://github.com/jibochu/wg/blob/main/WG.user.js
 // ==/UserScript==
