9931c8dc7e5e98a396ea3780037c7b8adc2d8c6a
jibochu <wangsicong360@gmail.com>
2026-08-07 20:51:17 +0800

    Create my-script.meta.js

---
+++ my-script.meta.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name         WG
+// @namespace    WG
+// @version      2.0.2
+// @downloadURL  https://ghproxy.net/https://github.com/jibochu/wg/blob/main/WG.user.js
+// ==/UserScript==
